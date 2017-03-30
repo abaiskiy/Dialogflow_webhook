b2b2045cf9f69046bf8273620059fbfe1c1a3e26
abaiskiy <abay.zhazbulganov@gmail.com>
2017-03-30 19:24:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -22,18 +22,27 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
-
+    #res = processRequest(req)
+    res = test()
+    
     res = json.dumps(res, indent=4)
-    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
+
+def test():
+    speech = "Some shit"
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
 
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
