d112c544eaa9e33bb44a1019c709e8e7822eff40
abaiskiy <abay.zhazbulganov@gmail.com>
2017-03-30 19:34:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -34,20 +34,12 @@ def webhook():
     return r
 
 
-def test():
-    
-    import requests
+def test():    
     s_city = "Petersburg,RU"
     appid = "01e9d712127bbffa4c9e669f39d3a127"
     
-    try:
-        result = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                    params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
-        data = res.json()
-        grad = data['weather'][0]['description']
-    except Exception as e:
-        grad = "problem"
-        pass
+    grad = "dermo"
+
 
     speech = grad
     return {
