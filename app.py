377daecdae6dc7145830afab31187532db980292
abaiskiy <abay.zhazbulganov@gmail.com>
2017-03-31 14:19:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def test():
         rez = requests.get("http://api.openweathermap.org/data/2.5/find",
                  params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
         #data = str(rez.json())
-        #data = rez.json()
-        speech = rez.list.name
+        data = rez.json()
+        speech = rez['main']['temp_min']
     except Exception as e:
         data = "Houston we have problem " + str(e)
         pass
