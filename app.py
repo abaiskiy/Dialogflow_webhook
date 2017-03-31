3af2718642d30f75f0709c40a45bb64975ec1e12
abaiskiy <abay.zhazbulganov@gmail.com>
2017-03-31 11:55:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -38,20 +38,18 @@ def test():
     s_city = "Almaty"
     appid = "01e9d712127bbffa4c9e669f39d3a127"
     
-    grad = "dermo"
     try:
-        #rez = requests.get("http://api.openweathermap.org/data/2.5/find?q=Almaty&units=metric&type=like&APPID=01e9d712127bbffa4c9e669f39d3a127&lang=ru")
         rez = requests.get("http://api.openweathermap.org/data/2.5/find",
                  params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
-        data = str(rez.json())
-        #data = json.loads(rez.text)
-        #grad = str(data) + "***"
-        #grad = data[0]['description']
+        #data = str(rez.json())
+        data = JSON.parse(rez)
+        speech = data.list.name
     except Exception as e:
-        grad = "Houston we have problem " + str(e)
+        data = "Houston we have problem " + str(e)
         pass
-    
+
     speech = data
+    
     return {
         "speech": speech,
         "displayText": speech,
