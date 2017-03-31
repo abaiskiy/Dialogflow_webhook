dcb776a3e80c79212fadfd7210b590fca6db3563
abaiskiy <abay.zhazbulganov@gmail.com>
2017-03-31 17:28:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,7 @@ def test():
                  params={'q': s_city, 'type': 'like', 'lang': lang, 'units': 'metric', 'APPID': appid})        
         #data = json.loads(rez)
         data = rez.json()
-        temp = "***"
-        #temp = data['list'][0]['main']['temp']
+        temp = data['list'][0]['main'][0]['temp']
         speech = data['list'][0]['weather'][0]['description'] +" "+ temp 
     except Exception as e:
         speech = "Houston we have problem " + str(e)
