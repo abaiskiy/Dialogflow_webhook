558ed139ce077df85f1b3779e2a4dcacde5aa4e6
abaiskiy <abay.zhazbulganov@gmail.com>
2017-04-03 12:21:47 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -53,8 +53,7 @@ def test(req):
         temp = str(data['list'][0]['main']['temp'])
         description = data['list'][0]['weather'][0]['description']
 		
-		x = float(temp)
-		y = round(x)
+		y = round(11.2)
 		z = str(y)
 		
         speech = u"Сегодня в "+s_city+" "+description+ u", температура "+temp + u" °C" + z
