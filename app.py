3df54a986f68312846e89480f232e8fe9a8fec90
abaiskiy <abay.zhazbulganov@gmail.com>
2017-04-24 20:18:34 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -45,13 +45,14 @@ def getService(req):
     elif action=="translate.text":
         return serviceTranslate(result)
     elif action=="dar.wiki":
-        return serviceWiki(result)    
+        return serviceWiki(result)
 
 def serviceWiki(result):
 
     parameters = result.get("parameters")
     text = parameters.get("text")
-    res = requests.get("https://ru.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exsentences=3&explaintext&titles="+text)
+    res = requests.get("https://ru.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&indexpageids=true&format=json&generator=search&gsrlimit=1&exsentences=3&gsrsearch="+text)
+    #res = requests.get("https://ru.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exsentences=3&explaintext&titles="+text)
     data = res.json()
     speech = data['query']['pages'].values()[0]['extract']
     return {
